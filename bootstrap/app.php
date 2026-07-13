939b91b49caf0bbe78226e07bbbed05c46f73369
Gansputra <ganangsetyaputra2@gmail.com>
2026-07-13 10:44:18 +0700

    Clean bootstrap configuration for Vercel env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,27 +20,4 @@ $app = Application::configure(basePath: dirname(__DIR__))
         //
     })->create();
 
-/*
-|--------------------------------------------------------------------------
-| Vercel Serverless Config
-|--------------------------------------------------------------------------
-| Redirect Laravel storage path to /tmp which is writable on Vercel
-*/
-if (isset($_ENV['VERCEL'])) {
-    $storagePath = '/tmp/storage';
-    $app->useStoragePath($storagePath);
-    
-    $dirs = [
-        $storagePath . '/framework/views',
-        $storagePath . '/framework/cache',
-        $storagePath . '/framework/sessions',
-        $storagePath . '/logs'
-    ];
-    foreach ($dirs as $dir) {
-        if (!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
-    }
-}
-
 return $app;
