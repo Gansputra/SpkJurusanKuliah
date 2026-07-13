f5f53feba16eb5e34d7ef7eb14d080b7c2f3e8dd
Gansputra <ganangsetyaputra2@gmail.com>
2026-07-13 11:29:50 +0700

    Add error catch to entrypoint

--- api/index.php
+++ api/index.php
@@ -6,11 +6,20 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Register the Composer autoloader...
-require __DIR__ . '/../vendor/autoload.php';
+try {
+    // Register the Composer autoloader...
+    require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+    // Bootstrap Laravel and handle the request...
+    /** @var Application $app */
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+    $app->handleRequest(Request::capture());
+} catch (\Throwable $e) {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo "<h1>FATAL ERROR: APPLICATION CRASHED</h1>";
+    echo "<h3>" . htmlspecialchars($e->getMessage()) . "</h3>";
+    echo "<p>File: <strong>" . htmlspecialchars($e->getFile()) . "</strong> on line <strong>" . $e->getLine() . "</strong></p>";
+    echo "<pre style='background: #f4f4f4; padding: 15px; border: 1px solid #ddd; overflow: auto;'>" . htmlspecialchars($e->getTraceAsString()) . "</pre>";
+    exit;
+}
