daeca4bbd4a5bced72bf218578a871b58067cb59
Gansputra <ganangsetyaputra2@gmail.com>
2026-07-13 10:49:36 +0700

    Bootstrap Laravel directly in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,15 @@
 <?php
 
-// Forward all incoming Vercel requests to Laravel's public entrypoint
-require __DIR__ . '/../public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
